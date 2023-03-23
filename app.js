76cc3f957efea513855b17ea69364013ecbd47f1
davidcreates265 <davidcreatesmw@gmail.com>
2023-03-23 07:02:31 +0200

    simple general todolist complete

--- app.js
+++ app.js
@@ -3,42 +3,38 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const app = express();  
- /** This function represents the express module & use the word'app' for best practice*/
+const app = express();
+/** This function represents the express module & use the word'app' for best practice*/
 
 let items = [];
 
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", function (request, response) {
+  let today = new Date();
 
-app.get("/", function(request, response){
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
 
-    let today = new Date();
-    
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
+  let day = today.toLocaleDateString("en-US", options);
 
-    let day = today.toLocaleDateString("en-US", options);
+  response.render("todolist", { whatDay: day, newListItems: items });
+});
 
-    response.render("todolist", {whatDay: day, newListItems: items});
-    });
+app.post("/", function (request, response) {
+  let item = request.body.newItem;
 
-    app.post("/", function(request, response){
-       let item = request.body.newItem;
-       
-       items.push(item);
+  items.push(item);
 
-       response.redirect("/");
+  response.redirect("/");
+});
 
-    })
-
-
-app.listen(3000, function(){
-    console.log("Server is listening or has started on specified port");
+app.listen(3000, function () {
+  console.log("Server is listening or has started on specified port");
 }); /** Use the app by using one of its method called listen. Listen on a specific port for any http request that gets sent to our server*/
